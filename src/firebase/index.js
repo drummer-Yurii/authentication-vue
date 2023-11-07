a8aa2ab41a4c9ba1d66fc0cad3cb40c10b7060cc
Yurii Rybachok <drummer2009@ukr.net>
2023-11-07 14:54:13 +0200

    setup project

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDBCtzDvUyidGdFNQHSE4AKlL0wTluM6u8',
+  authDomain: 'authentication-vue-fc09c.firebaseapp.com',
+  projectId: 'authentication-vue-fc09c',
+  storageBucket: 'authentication-vue-fc09c.appspot.com',
+  messagingSenderId: '39929267547',
+  appId: '1:39929267547:web:7e9cba609dc7fe4445067e',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth };
