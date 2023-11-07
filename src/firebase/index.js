260de3acdb1fbf35dedbc15c350a40d71e24ab3e
Yurii Rybachok <drummer2009@ukr.net>
2023-11-07 16:49:18 +0200

    add functions fro register & login

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDBCtzDvUyidGdFNQHSE4AKlL0wTluM6u8',
